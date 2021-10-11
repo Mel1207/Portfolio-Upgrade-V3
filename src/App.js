dfffa1ba1825d3881e16c43206a31d909008f6bf
Mel <meljohnpualon@gmail.com>
2021-10-12 03:48:56 +0800

    added section header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div>
       <h1>FRONT_END DEVELOPER</h1>
       <h1>UI/UX DESIGNER</h1>
+      <h2 className="section-header">Hi There I'm MJ</h2>
       <a href="#!" className="btn">See Works</a>
     </div>
   )
