629071b25dca21dc331db6da6eeb2f7724407516
Mel <meljohnpualon@gmail.com>
2021-10-12 04:11:03 +0800

    added container global

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './sass/main.css';
 function App() {
   return (
     <div>
-      <h1>FRONT_END DEVELOPER</h1>
-      <h1>UI/UX DESIGNER</h1>
-      <h2 className="section-header">Hi There I'm MJ</h2>
-      <a href="#!" className="btn">See Works</a>
+      <div className="container">
+        <h2 className="section-header">Hi There I'm MJ</h2>
+        <h1>FRONT_END DEVELOPER</h1>
+        <h1>UI/UX DESIGNER</h1>
+        <a href="#!" className="btn">See Works</a>
+      </div>
     </div>
   )
 }
