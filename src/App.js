70bcda2f5243f79d1d5972da07cc1e092dd18ccb
Mel <meljohnpualon@gmail.com>
2021-10-13 11:52:44 +0800

    added section1 component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Section1 from './components/Section-1';
 import './sass/main.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar />
       <Header />
+      <Section1 />
     </div>
   )
 }
---
+++ src/components/Section-1.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Section1 = () => {
+    return (
+        <div className="section-1">
+            <div className="container flex">
+                <div className="left-aside">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam tempora reiciendis quam, iusto omnis itaque accusamus? Voluptatibus doloremque voluptate non neque exercitationem minima ex, quasi eius, numquam quo tempora ducimus dolorum cupiditate ab nam iste culpa provident! Vitae recusandae expedita nam, sint quisquam porro hic exercitationem odio. Accusamus, maxime repellendus.</div>
+                <div className="right-aside">
+                    <h2 className="section-header">What I Do ?</h2>
+                    <h1>I DESIGN AND BUILD BEAUTIFUL WEBSITES</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Section1
