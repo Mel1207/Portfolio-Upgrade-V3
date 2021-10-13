9e0c338521f75622952eddf1d935887bd5fb4445
Mel <meljohnpualon@gmail.com>
2021-10-14 07:09:40 +0800

    added footer base and aligment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,27 +14,31 @@ const Footer = () => {
             backgroundAttachment: 'fixed'
         }}>
             <div className="overlay">
-                <div className="container grid">
-                    <div>
-                        <h2 className="section-header">Lets get in touch</h2>
-                        <h3 className="footer-subheader">Need a website ?</h3>
+                <div className="container">
+                    <div className="grid">
+                        <div>
+                            <h2 className="section-header">Lets get in touch</h2>
+                            <h3 className="footer-subheader">Need a website ?</h3>
 
-                        <ul className="footer-links">
-                            <li><a href="#!"><img src={navLinkIn} alt="" /></a></li>
-                            <li><a href="#!"><img src={navBehance} alt="" /></a></li>
-                            <li><a href="#!"><img src={navGithub} alt="" /></a></li>
-                            <li><a href="#!"><img src={navFacebook} alt="" /></a></li>
-                        </ul>
+                            <ul className="footer-links">
+                                <li><a href="#!"><img src={navLinkIn} alt="" /></a></li>
+                                <li><a href="#!"><img src={navBehance} alt="" /></a></li>
+                                <li><a href="#!"><img src={navGithub} alt="" /></a></li>
+                                <li><a href="#!"><img src={navFacebook} alt="" /></a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <form className="form-group form-container">
+                                <input type="text" placeholder="Your name" />
+                                <input type="text" placeholder="Your email" />
+                                <textarea name="" id="" cols="30" rows="10" placeholder="Your message"></textarea>
+                                <button className="btn" type="submit">Send</button>
+                            </form>
+                        </div>
                     </div>
-                    <div>
-                        <form className="form-group form-container">
-                            <input type="text" placeholder="Your name" />
-                            <input type="text" placeholder="Your email" />
-                            <textarea name="" id="" cols="30" rows="10" placeholder="Your message"></textarea>
-                            <button className="btn" type="submit">Send</button>
-                        </form>
-                    </div>
+                    <span className="footer-base">Copyright © 2021 | Mel John Pualon</span>
                 </div>
+                
             </div>
         </div>
     )
