8eb714315386a28f4ca4f676f97520ac436a4528
Mel <meljohnpualon@gmail.com>
2021-10-13 19:05:08 +0800

    fix feature logo and containers

--- src/components/Section-2.js
+++ src/components/Section-2.js
@@ -9,18 +9,24 @@ const Section2 = () => {
             <div className="container">
                 <h2 className="section-header">My Services</h2>
                 <div className="grid">
-                    <div>
-                        <img src={logoFrontEnd} alt="" />
+                    <div className="grid-container">
+                        <div className="container-logo">
+                            <img src={logoFrontEnd} alt="" />
+                        </div>
                         <h5>FRONT-END DEVELOPMENT</h5>
                         <p>I can build a dynamic website designs, creates an interaction and user experience. I can convert the design concept into a usable component.</p>
                     </div>
-                    <div>
-                        <img src={logoUiux} alt="" />
+                    <div className="grid-container">
+                        <div className="container-logo">
+                            <img src={logoUiux} alt="" />
+                        </div>  
                         <h5>UI/UX DESIGN</h5>
                         <p>I design a UI-UX to the extent of a project and can create a prototype that demonstrate the project features and user experience.</p>
                     </div>
-                    <div>
-                        <img src={logoResponsive} alt="" />
+                    <div className="grid-container">
+                        <div className="container-logo">
+                            <img src={logoResponsive} alt="" />
+                        </div>
                         <h5>RESPONSIVE DESIGN</h5>
                         <p>I make every project responsive and mobile friendly to the users. Making layouts will automatically adjust and adapt to any device screen size.</p>
                     </div>
