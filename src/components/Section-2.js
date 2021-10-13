740654533af8378b2bd157d6aea2b04bfbe5b337
Mel <meljohnpualon@gmail.com>
2021-10-13 14:12:23 +0800

    added section 2 features and styles

--- src/components/Section-2.js
+++ src/components/Section-2.js
@@ -4,7 +4,24 @@ const Section2 = () => {
     return (
         <div className="section-2">
             <div className="container">
-                
+                <h2 className="section-header">My Services</h2>
+                <div className="grid">
+                    <div>
+                        <img src="" alt="" />
+                        <h5>FRONT-END DEVELOPMENT</h5>
+                        <p>I can build a dynamic website designs, creates an interaction and user experience. I can convert the design concept into a usable component.</p>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <h5>FRONT-END DEVELOPMENT</h5>
+                        <p>I can build a dynamic website designs, creates an interaction and user experience. I can convert the design concept into a usable component.</p>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <h5>FRONT-END DEVELOPMENT</h5>
+                        <p>I can build a dynamic website designs, creates an interaction and user experience. I can convert the design concept into a usable component.</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
