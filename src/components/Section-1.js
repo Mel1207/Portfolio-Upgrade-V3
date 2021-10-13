884d299b7e722480161dcbbdc550a3b96ae1f662
Mel <meljohnpualon@gmail.com>
2021-10-13 12:09:03 +0800

    added section1 header, images and styles

--- src/components/Section-1.js
+++ src/components/Section-1.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Object1 from '../img/elevedge.png'
+import Object2 from '../img/MC-app-mobile.png'
 
 const Section1 = () => {
     return (
         <div className="section-1">
             <div className="container flex">
-                <div className="left-aside">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam tempora reiciendis quam, iusto omnis itaque accusamus? Voluptatibus doloremque voluptate non neque exercitationem minima ex, quasi eius, numquam quo tempora ducimus dolorum cupiditate ab nam iste culpa provident! Vitae recusandae expedita nam, sint quisquam porro hic exercitationem odio. Accusamus, maxime repellendus.</div>
+                <div className="left-aside">
+                    <div>
+                        <img src={Object1} alt="Design 1" className="object-1"/>
+                        <img src={Object2} alt="Design 2" className="object-2"/>
+                    </div>
+                </div>
                 <div className="right-aside">
                     <h2 className="section-header">What I Do ?</h2>
                     <h1>I DESIGN AND BUILD BEAUTIFUL WEBSITES</h1>
