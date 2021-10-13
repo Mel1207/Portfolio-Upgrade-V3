ab3a9f176259155c2e36b3ed5fcedd34cf3c42d4
Mel <meljohnpualon@gmail.com>
2021-10-14 04:32:50 +0800

    added pages and react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
