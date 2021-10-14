38d1dad258d73acd95a484bc712a32a669ee17ac
Mel <meljohnpualon@gmail.com>
2021-10-15 06:19:12 +0800

    fix path checker to prop checker

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,10 @@ import navBehance from '../img/nav-logo-behance.svg'
 import navLinkIn from '../img/nav-logo-linkedIn.svg'
 import navGithub from '../img/nav-logo-github.svg'
 import navFacebook from '../img/nav-logo-facebook.svg'
+import navBlackLinkIn from '../img/nav-logoBlack-linkedIn.svg'
+import navBlackBehance from '../img/nav-logoBlack-behance.svg'
+import navBlackGithub from '../img/nav-logoBlack-github.svg'
+import navBlackFacebook from '../img/nav-logoBlack-facebook.svg'
 
 const Navbar = (props) => {
     // console.log(props)
@@ -16,28 +20,29 @@ const Navbar = (props) => {
                     <img src={navLogo} alt="MjP Logo" />
                 </NavLink>
 
-                <ul className={!props.isExact ? 'changeStyledNav' : ''}>
+                {/* className={!props.isExact ? 'changeStyledNav' : ''} */}
+                <ul>
                     <li>
                         <Link to="/about">about</Link>
                     </li>
                     <li>
                         <a href="https://www.linkedin.com/in/mel-john-pualon-75345419a" target="_blank" rel="noreferrer">
-                            <img src={navLinkIn} alt="LinkedIn" />
+                            <img src={!props.isExact ? navBlackLinkIn : navLinkIn } alt="LinkedIn" />
                         </a>
                     </li>
                     <li>
                         <a href="https://www.behance.net" target="_blank" rel="noreferrer">
-                            <img src={navBehance} alt="Behance" />
+                            <img src={!props.isExact ? navBlackBehance : navBehance } alt="Behance" />
                         </a>
                     </li>
                     <li>
                         <a href="https://github.com/Mel1207" target="_blank" rel="noreferrer">
-                            <img src={navGithub} alt="Github" />
+                            <img src={!props.isExact ? navBlackGithub : navGithub} alt="Github" />
                         </a>
                     </li>
                     <li>
                         <a href="https://www.facebook.com/Meljohn.pualon" target="_blank" rel="noreferrer">
-                            <img src={navFacebook} alt="Facebook" />
+                            <img src={!props.isExact ? navBlackFacebook : navFacebook} alt="Facebook" />
                         </a>
                     </li>
                 </ul>
