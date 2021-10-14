cd8ea2dd39f83cb3ab96e097c7e9b5e645bd5b06
Mel <meljohnpualon@gmail.com>
2021-10-15 04:27:11 +0800

    fix navlinks for social media

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import navLogo from '../img/nav-logo-main.svg'
 import navBehance from '../img/nav-logo-behance.svg'
 import navLinkIn from '../img/nav-logo-linkedIn.svg'
@@ -9,28 +10,28 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="container nav-flex">
-                <a href="#!">
+                <NavLink to="/" exact>
                     <img src={navLogo} alt="MjP Logo" />
-                </a>
+                </NavLink>
 
                 <ul>
                     <li>
-                        <a href="#!">
+                        <a href="https://www.linkedin.com/in/mel-john-pualon-75345419a" target="_blank" rel="noreferrer">
                             <img src={navLinkIn} alt="LinkedIn" />
                         </a>
                     </li>
                     <li>
-                        <a href="#!">
+                        <a href="https://www.behance.net" target="_blank" rel="noreferrer">
                             <img src={navBehance} alt="Behance" />
                         </a>
                     </li>
                     <li>
-                        <a href="#!">
+                        <a href="https://github.com/Mel1207" target="_blank" rel="noreferrer">
                             <img src={navGithub} alt="Github" />
                         </a>
                     </li>
                     <li>
-                        <a href="#!">
+                        <a href="https://www.facebook.com/Meljohn.pualon" target="_blank" rel="noreferrer">
                             <img src={navFacebook} alt="Facebook" />
                         </a>
                     </li>
