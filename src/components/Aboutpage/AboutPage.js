0a0ad9e286dbac1b356bb6107fba526b1cb918a9
Mel <meljohnpualon@gmail.com>
2021-10-17 05:11:15 +0800

    added responsive fix for cards

--- src/components/Aboutpage/AboutPage.js
+++ src/components/Aboutpage/AboutPage.js
@@ -3,6 +3,11 @@ import Navbar from '../Navbar'
 import aboutBg from '../../img/aboutBg.png'
 import aboutAvatar from '../../img/avatar.jpg'
 import galleryImg from '../../img/gallery-img.jpg'
+import iconJavascript from '../../img/icon-js.svg'
+import iconReact from '../../img/icon-react.svg'
+import iconJquery from '../../img/icon-jquery.svg'
+import iconHtml from '../../img/icon-html.svg'
+import iconCss from '../../img/icon-css.svg'
 
 const AboutPage = () => {
     
@@ -37,10 +42,141 @@ const AboutPage = () => {
             </div>
             <div className="skills-section">
                 <div className="container">
+                    <h2 className="section-header">Technical skills</h2>
+                    <h3 className="section-subheader">My top technologies</h3>
                     <div className="skills-card-grid">
-                        <div className="skills-card">1</div>
-                        <div className="skills-card">2</div>
-                        <div className="skills-card">3</div>
+                        <div className="skills-card">
+                            <div className="skills-img-container" style={{
+                                padding: '1.5rem',
+                                background: `url("${galleryImg}")`,
+                                height: '150px',
+                                width: '100%',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                position: 'relative'
+                            }}>
+                                <h4>FRONT-END</h4>
+                            </div>
+                            <div className="skills-card-content">
+                                <ul>
+                                    <li>
+                                        <span><img src={iconJavascript} alt="Javascript" />Javascript</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconReact} alt="React.js" />React.js</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconJquery} alt="Jquery" />Jquery</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconHtml} alt="HTML5" />HTML5</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconCss} alt="CSS3" />CSS3</span>
+                                    </li>
+                                   
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="skills-card">
+                            <div className="skills-img-container" style={{
+                                padding: '1.5rem',
+                                background: `url("${galleryImg}")`,
+                                height: '150px',
+                                width: '100%',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                position: 'relative'
+                            }}>
+                                <h4>FRONT-END</h4>
+                            </div>
+                            <div className="skills-card-content">
+                                <ul>
+                                    <li>
+                                        <span><img src={iconJavascript} alt="Javascript" />Javascript</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconReact} alt="React.js" />React.js</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconJquery} alt="Jquery" />Jquery</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconHtml} alt="HTML5" />HTML5</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconCss} alt="CSS3" />CSS3</span>
+                                    </li>
+                                   
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="skills-card">
+                            <div className="skills-img-container" style={{
+                                padding: '1.5rem',
+                                background: `url("${galleryImg}")`,
+                                height: '150px',
+                                width: '100%',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                position: 'relative'
+                            }}>
+                                <h4>FRONT-END</h4>
+                            </div>
+                            <div className="skills-card-content">
+                                <ul>
+                                    <li>
+                                        <span><img src={iconJavascript} alt="Javascript" />Javascript</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconReact} alt="React.js" />React.js</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconJquery} alt="Jquery" />Jquery</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconHtml} alt="HTML5" />HTML5</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconCss} alt="CSS3" />CSS3</span>
+                                    </li>
+                                   
+                                </ul>
+                            </div>
+                        </div>
+                        {/* <div className="skills-card">
+                            <div className="skills-img-container" style={{
+                                padding: '1.5rem',
+                                background: `url("${galleryImg}")`,
+                                height: '150px',
+                                width: '100%',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                position: 'relative'
+                            }}>
+                                <h4>FRONT-END</h4>
+                            </div>
+                            <div className="skills-card-content">
+                                <ul>
+                                    <li>
+                                        <span><img src={iconJavascript} alt="Javascript" />Javascript</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconReact} alt="React.js" />React.js</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconJquery} alt="Jquery" />Jquery</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconHtml} alt="HTML5" />HTML5</span>
+                                    </li>
+                                    <li>
+                                        <span><img src={iconCss} alt="CSS3" />CSS3</span>
+                                    </li>
+                                   
+                                </ul>
+                            </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
