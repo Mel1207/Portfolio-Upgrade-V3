19f860770eb81fad50c0fc16937b636ff52545ad
Mel <meljohnpualon@gmail.com>
2021-10-17 00:42:49 +0800

    added hero banner for about page

--- src/components/Aboutpage/AboutPage.js
+++ src/components/Aboutpage/AboutPage.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import Navbar from '../Navbar'
+import aboutBg from '../../img/aboutBg.png'
+import aboutAvatar from '../../img/avatar.jpg'
 
-const AbougPage = () => {
-    
+const AboutPage = () => {
     
     return (
         <>
             <Navbar />
+            <div className="about-bg" style={{
+                background: `url("${aboutBg}")`,
+                height: '60vh',
+                width: '100%',
+                display: 'block'
+            }}></div>
             <div className="container">
-                <h1>THIS IS ABOUT PAGE</h1>
+                <div className="about-grid">
+                    <img src={aboutAvatar} alt="Mel John"  className="about-avatar-img"/>
+                    <div>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam necessitatibus temporibus explicabo quidem atque maxime quam mollitia assumenda maiores sint!
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error eum ea suscipit! Quis consequatur rerum vero, tempora voluptate minus sed.
+                    </div>
+                </div>
             </div>
             
         </>
     )
 }
 
-export default AbougPage
+export default AboutPage
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import navLogo from '../img/nav-logo-main.svg'
 import navBehance from '../img/nav-logo-behance.svg'
 import navLinkIn from '../img/nav-logo-linkedIn.svg'
