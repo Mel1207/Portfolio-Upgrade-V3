7320be6e5d25f5eeee8109f20067fbd6a73af308
Mel <meljohnpualon@gmail.com>
2021-10-20 06:25:18 +0800

    added return routerlink to blog pages

--- src/components/SingleBlogPage/SingleBlogPage.js
+++ src/components/SingleBlogPage/SingleBlogPage.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../Navbar'
+import { Link } from 'react-router-dom'
 
 const SingleBlogPage = () => {
     return (
         <>
             <Navbar />
             <div className="container">
+                
                 <h1>THIS IS SINGLE BLOG PAGE</h1>
+                <Link to="/blog/">Back to blogs</Link>
             </div>
         </>
     )
