6f028b212f2bf247bf48c20aacaf98d91e40be44
Mel <meljohnpualon@gmail.com>
2021-10-19 16:21:00 +0800

    added blogs pages title

--- src/components/Blogpage/BlogPage.js
+++ src/components/Blogpage/BlogPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import aboutBg from '../../img/aboutBg.png'
 import Navbar from '../Navbar'
 // import sanityClient from "../../client"
 
@@ -16,6 +17,21 @@ const BlogPage = () => {
     return (
         <>  
             <Navbar />
+            <div className="about-bg" style={{
+                background: `url("${aboutBg}")`,
+                backgroundAttachment: 'fixed',
+                backgroundRepeat: 'no-repeat',
+                height: '60vh',
+                width: '100%',
+                display: 'block'
+            }}>
+                <div className="container" style={{position: 'relative',height: '100%'}}>
+                    <div className="page-title">
+                        <h2 className="section-header">My Blogs</h2>
+                        <h3 className="section-subheader">Take a time to read my blogs</h3>
+                    </div>
+                </div>
+            </div>
             <div className="container">
                 <h1>THIS IS BLOG PAGE</h1>
             </div>
