475e9bdb26c43697bfc3ec3afd3ad0b73e6a29fe
Mel <meljohnpualon@gmail.com>
2021-10-20 10:01:52 +0800

    added single blog page bg container

--- src/components/SingleBlogPage/SingleBlogPage.js
+++ src/components/SingleBlogPage/SingleBlogPage.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Navbar from '../Navbar'
 import { Link } from 'react-router-dom'
+import aboutBg from '../../img/aboutBg.png'
 
 const SingleBlogPage = () => {
     return (
         <>
             <Navbar />
+            <div className="about-bg" style={{
+                background: `url("${aboutBg}")`,
+                backgroundAttachment: 'fixed',
+                backgroundRepeat: 'no-repeat',
+                height: '60vh',
+                width: '100%',
+                display: 'block'
+            }}></div>
             <div className="container">
-                
                 <h1>THIS IS SINGLE BLOG PAGE</h1>
                 <Link to="/blog/">Back to blogs</Link>
             </div>
