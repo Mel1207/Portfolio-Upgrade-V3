6c21c4a8fa896e9df8fc5fc1a5a40fc1e8ef3c15
Mel <meljohnpualon@gmail.com>
2021-10-20 11:30:37 +0800

    fix main image fetcher on single blog

--- src/components/SingleBlogPage/SingleBlogPage.js
+++ src/components/SingleBlogPage/SingleBlogPage.js
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import sanityClient from '../../client'
+import { useParams } from 'react-router'
+import imageUrlBuilder from '@sanity/image-url'
 import Navbar from '../Navbar'
 import { Link } from 'react-router-dom'
-import aboutBg from '../../img/aboutBg.png'
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source)
+}
 
 const SingleBlogPage = () => {
+    const [singleBlog, setSingleBlog] = useState(null)
+    const { slug } = useParams()
+
+    useEffect(() => {
+        sanityClient.fetch(`*[slug.current == "${slug}"]{
+            title,
+            _id, 
+            slug,
+            mainImage{
+                asset->{
+                    _id,
+                    url
+                },
+            },
+            body,
+            "name": author->name,
+            "authorImage": author->image
+        }`).then(data => setSingleBlog(data[0])).catch(console.error)
+    }, [slug])
+    
+
+    if(!singleBlog) return <div><h1>Loading...</h1></div>
+
     return (
         <>
             <Navbar />
-            <div className="about-bg" style={{
-                background: `url("${aboutBg}")`,
+            <div className="blog-bg" style={{
+                background: `url("${singleBlog.mainImage.asset.url}")`,
                 backgroundAttachment: 'fixed',
                 backgroundRepeat: 'no-repeat',
                 height: '60vh',
                 width: '100%',
-                display: 'block'
-            }}></div>
-            <div className="container">
-                <h1>THIS IS SINGLE BLOG PAGE</h1>
+                }}>
+            </div>
+            <div className="container blog-container">
+                <img className="blog-mainTop-img" src={singleBlog.mainImage.asset.url} />
+                <div className="blog-body">
+                    <h1>This is block content</h1>
+                </div>
                 <Link to="/blog/">Back to blogs</Link>
             </div>
         </>
