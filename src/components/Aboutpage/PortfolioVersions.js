6a034977446387cc03a301b24a8435c559418e89
Mel <meljohnpualon@gmail.com>
2021-10-23 07:45:32 +0800

    fix portfolio versions ui and grid responsive

--- src/components/Aboutpage/PortfolioVersions.js
+++ src/components/Aboutpage/PortfolioVersions.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import sanityClient from '../../client'
+import iconLink from '../../img/icon-link.svg'
+import iconLinkGithub from '../../img/nav-logoBlack-github.svg'
 
 const PortfolioVersions = () => {
     const [portfolioVer, setPortfolioVer] = useState(null)
@@ -26,13 +28,25 @@ const PortfolioVersions = () => {
     return (
         <div className="portfolio-version-section">
             <div className="container">
-                <h2 className="section-header">Portfolio</h2>
-                <h3 className="section-subheader">Take a look at my projects</h3>
+                <h2 className="section-header">Previous versions</h2>
+                <h3 className="section-subheader">Checkout my porfolio versions</h3>
                 
-                <div className="portfolioVer-continer">
+                <div >
                     {portfolioVer && portfolioVer.map((pVersions, index) => (
-                        <div>
-                            <h3>{pVersions.title}</h3>
+                        <div key={index} className="portfolioVer-continer">
+                            <img src={pVersions.mainImage.asset.url} alt={pVersions.title} />
+                            <div className="portfolioVer-content">
+                                <a href={pVersions.link} target="_blank" rel="noopener noreferrer" alt={pVersions.title} className="project-name">{pVersions.title}</a>
+                                <span className="project-date">Published: <strong>{ new Date(pVersions.date).toDateString()}</strong></span>
+                                <p className="project-description">{pVersions.description}</p>
+                                <p className="project-tech">Technology used:</p>
+                                <ul><li key={pVersions.tags}>{pVersions.tags}</li></ul>
+                                <div className="project-navigation-links">
+                                    <a href={pVersions.link} rel="noopener noreferrer" target="_blank" rel="noopener noreferrer" className="project-link"><img src={iconLink} alt="link"  />Website link</a>
+                                    <a href={pVersions.repoLink} rel="noopener noreferrer" className="repository-link"><img src={iconLinkGithub} alt="link"  />Repository
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     ))}
                     
