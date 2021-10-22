a95c99d4a2ad07ff0074073647e71e067f2c2fdd
Mel <meljohnpualon@gmail.com>
2021-10-22 15:11:24 +0800

    fix section spacing

--- src/components/Aboutpage/PortfolioVersions.js
+++ src/components/Aboutpage/PortfolioVersions.js
@@ -4,6 +4,8 @@ const PortfolioVersions = () => {
     return (
         <div className="portfolio-version-section">
             <div className="container">
+                <h2 className="section-header">Portfolio</h2>
+                <h3 className="section-subheader">Take a look at my projects</h3>
                 <h1>this is portfolio collections</h1>
             </div>
         </div>
