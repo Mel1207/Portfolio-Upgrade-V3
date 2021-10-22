35ba52bbf829509d0c60a5433d2b9d733237d842
Mel <meljohnpualon@gmail.com>
2021-10-23 01:18:15 +0800

    success linked schema to fetch api

--- src/components/Aboutpage/PortfolioVersions.js
+++ src/components/Aboutpage/PortfolioVersions.js
@@ -5,15 +5,38 @@ const PortfolioVersions = () => {
     const [portfolioVer, setPortfolioVer] = useState(null)
 
     useEffect(() => {
-        sanityClient.fetch(`*[_]`)
-    })
+        sanityClient.fetch(`*[_type == "portfolioVer"]{
+            title,
+            date,
+            mainImage{
+                asset->{
+                    _id,
+                    url
+                },
+                alt
+            },
+            description,
+            projectType,
+            link,
+            repoLink,
+            tags
+        }`).then(data => setPortfolioVer(data)).catch(console.error);
+    }, []);
 
     return (
         <div className="portfolio-version-section">
             <div className="container">
                 <h2 className="section-header">Portfolio</h2>
                 <h3 className="section-subheader">Take a look at my projects</h3>
-                <h1>this is portfolio collections</h1>
+                
+                <div className="portfolioVer-continer">
+                    {portfolioVer && portfolioVer.map((pVersions, index) => (
+                        <div>
+                            <h3>{pVersions.title}</h3>
+                        </div>
+                    ))}
+                    
+                </div>
             </div>
         </div>
     )
