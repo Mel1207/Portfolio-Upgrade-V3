2806e495cc3bc7a3aa817ca7609480801065ab75
Mel <meljohnpualon@gmail.com>
2021-10-24 11:16:29 +0800

    fix section 1 grid

--- src/components/Homepage/Section-1.js
+++ src/components/Homepage/Section-1.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import Object1 from '../../img/elevedge.png'
+import Object1 from '../../img/img-object.png'
 import Object2 from '../../img/MC-app-mobile.png'
 // comment
 
 const Section1 = () => {
     return (
         <div className="section-1" id="section-1">
-            <div className="container flex">
+            <div className="container grid">
                 <div className="left-aside">
                     <div>
                         <img src={Object1} alt="Design 1" className="object-1"/>
-                        <img src={Object2} alt="Design 2" className="object-2"/>
+                        {/* <img src={Object2} alt="Design 2" className="object-2"/>   */}
                     </div>
                 </div>
                 <div className="right-aside">
