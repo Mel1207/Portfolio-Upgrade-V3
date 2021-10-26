ff930e37211fcf753609bddfa8b92856c20196fe
Mel <meljohnpualon@gmail.com>
2021-10-26 09:42:14 +0800

    fix email sendler and input clearer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,34 @@ import navBehance from '../img/nav-logo-behance.svg'
 import navLinkIn from '../img/nav-logo-linkedIn.svg'
 import navGithub from '../img/nav-logo-github.svg'
 import navFacebook from '../img/nav-logo-facebook.svg'
+import emailjs from 'emailjs-com'
 
 const Footer = () => {
+    const handleSendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            'service_0c1xddh', 
+            'template_owlqxmf', 
+            e.target, 
+            'user_8eChwnD42VoCAsL3qRkT5'
+        ).then(res => {
+            console.log(res.status)
+
+        }).catch(console.error)
+
+        document.querySelector('#inputName').value = ''
+        document.querySelector('#inputEmail').value = ''
+        document.querySelector('#inputMessage').value = ''
+    }
+
+    // const handleNullValue = (e) => {
+    //     console.log('cleared text')
+    //     // document.querySelector('#inputName').value = ''
+    //     // document.querySelector('#inputEmail').value = ''
+    //     // document.querySelector('#inputMessage').value = ''
+    // }
+
     return (
         <div className="section-footer" style={{
             background: `url("${footerBg}")`,
@@ -28,18 +54,20 @@ const Footer = () => {
                             </ul>
                         </div>
                         <div>
-                            <form className="form-group form-container">
-                                <input type="text" placeholder="Your name" />
-                                <input type="text" placeholder="Your email" />
-                                <textarea name="" id="" cols="30" rows="10" placeholder="Your message"></textarea>
+                            <form className="form-group form-container" onSubmit={handleSendEmail}>
+                                <input type="text" name="name" placeholder="Your name" id="inputName"/>
+                                <input type="text" name="sender-email" placeholder="Your email" id="inputEmail"/>
+                                <textarea cols="30" rows="10" placeholder="Your message" name="message" id="inputMessage"></textarea>
                                 <button className="btn" type="submit">Send</button>
                             </form>
                         </div>
                     </div>
                     <span className="footer-base">Copyright © 2021 <span>|</span> Mel John Pualon</span>
                 </div>
-                
             </div>
+            {/* <div className="showMessage">
+                <span>Your Email has been sent thank you!</span>
+            </div> */}
         </div>
     )
 }
