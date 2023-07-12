53eb28a37b99d1570dbd8f7c2e41d9ece925c283
Mel <meljohnpualon@gmail.com>
2023-07-12 16:03:11 +0800

    fix overall page background

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,71 +1,148 @@
-import React, { useState, useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
-import navLogo from '../img/nav-logo-main.svg'
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import navLogo from "../img/nav-logo-main.svg";
 
 const Navbar = (props) => {
-    const [addClass, setAddClass] = useState(false)
+  const [addClass, setAddClass] = useState(false);
 
-    const handleShowMenu = () => {
-        setAddClass(!addClass)
-        document.body.classList.toggle('body-hidden')
+  const handleShowMenu = () => {
+    setAddClass(!addClass);
+    document.body.classList.toggle("body-hidden");
+  };
+
+  const handleRemoveClass = () => {
+    document.body.classList.remove("body-hidden");
+    window.scrollTo({
+      top: 0,
+      behavior: "auto",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkClassNav);
+  }, []);
+
+  const [scrollNavClass, setScrollNavClass] = useState(false);
+
+  const checkClassNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNavClass("navbar-stickTop");
+    } else {
+      setScrollNavClass(null);
     }
-    
-    const handleRemoveClass = () => {
-        document.body.classList.remove('body-hidden')
-        window.scrollTo({
-            top: 0,
-            behavior: 'auto'
-        })
-    }
+  };
 
-    useEffect(() => {
-        window.addEventListener('scroll', checkClassNav)
-    }, [])
+  // console.log(props)
 
-    const [scrollNavClass, setScrollNavClass] = useState(false)
+  return (
+    <nav
+      className={`navbar ${scrollNavClass}`}
+      style={{ background: !props.isExact ? "#fbfbfb" : "transparent" }}
+    >
+      <div className="container nav-flex">
+        <NavLink to="/" exact>
+          <img src={navLogo} alt="MjP Logo" className="nav-logo" />
+        </NavLink>
+        <ul className="nav-links">
+          <li>
+            <NavLink
+              to="/"
+              style={{ color: !props.isExact ? "#353535" : "#fff" }}
+              onClick={handleRemoveClass}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              style={{ color: !props.isExact ? "#353535" : "#fff" }}
+              onClick={handleRemoveClass}
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/portfolio"
+              style={{ color: !props.isExact ? "#353535" : "#fff" }}
+              onClick={handleRemoveClass}
+            >
+              Portfolio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/blog"
+              style={{ color: !props.isExact ? "#353535" : "#fff" }}
+              onClick={handleRemoveClass}
+            >
+              Blogs
+            </NavLink>
+          </li>
+        </ul>
 
-    const checkClassNav = () => {
-        if(window.scrollY >= 80) {
-            setScrollNavClass('navbar-stickTop')
-        } else {
-            setScrollNavClass(null)
-        }
-    }
+        <div
+          className={`hamburger-menu ${addClass ? "menu-click" : null}`}
+          onClick={handleShowMenu}
+        >
+          <div className="top-line"></div>
+          <div className="mid-line"></div>
+          <div className="bot-line"></div>
+        </div>
 
-    // console.log(props)
-
-    return (
-        <nav className={`navbar ${scrollNavClass}`} style={{background: !props.isExact ? '#fbfbfb' : 'transparent'}}>
-            <div className="container nav-flex">
-                <NavLink to="/" exact>
-                    <img src={navLogo} alt="MjP Logo" className="nav-logo"/>
+        <div className={`menu ${addClass ? "menu-show" : null}`}>
+          <div className="container">
+            <ul className="">
+              <li>
+                <NavLink
+                  to="/"
+                  style={{ color: "#353535" }}
+                  onClick={handleRemoveClass}
+                >
+                  <span>1.<p>Home</p>
+                  </span>
                 </NavLink>
-                <ul className="nav-links">
-                    <li><NavLink to="/" style={{color: !props.isExact ? '#353535' : '#fff'}} onClick={handleRemoveClass}>Home</NavLink></li>
-                    <li><NavLink to="/about" style={{color: !props.isExact ? '#353535' : '#fff'}} onClick={handleRemoveClass}>About</NavLink></li>
-                    <li><NavLink to="/portfolio" style={{color: !props.isExact ? '#353535' : '#fff'}} onClick={handleRemoveClass}>Portfolio</NavLink></li>
-                    <li><NavLink to="/blog" style={{color: !props.isExact ? '#353535' : '#fff'}} onClick={handleRemoveClass}>Blogs</NavLink></li>
-                </ul>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  style={{ color: "#353535" }}
+                  onClick={handleRemoveClass}
+                >
+                  <span>
+                    2.<p>About</p>
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/portfolio"
+                  style={{ color: "#353535" }}
+                  onClick={handleRemoveClass}
+                >
+                  <span>
+                    3.<p>Portfolio</p>
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/blog"
+                  style={{ color: "#353535" }}
+                  onClick={handleRemoveClass}
+                >
+                  <span>
+                    4.<p>Blogs</p>
+                  </span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-                <div className={`hamburger-menu ${addClass ? 'menu-click' : null}`} onClick={handleShowMenu}>
-                    <div className="top-line"></div>
-                    <div className="mid-line"></div>
-                    <div className="bot-line"></div>
-                </div>
-
-                <div className={`menu ${addClass ? 'menu-show' : null}`}>
-                    <div className="container">
-                        <ul className="">
-                            <li><NavLink to="/" style={{color: '#353535'}} onClick={handleRemoveClass}><span>1.<p>Home</p></span></NavLink></li>
-                            <li><NavLink to="/about" style={{color: '#353535'}} onClick={handleRemoveClass}><span>2.<p>About</p></span></NavLink></li>
-                            <li><NavLink to="/portfolio" style={{color: '#353535'}} onClick={handleRemoveClass}><span>3.<p>Portfolio</p></span></NavLink></li>
-                            <li><NavLink to="/blog" style={{color: '#353535'}} onClick={handleRemoveClass}><span>4.<p>Blogs</p></span></NavLink></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
-}
-
-export default Navbar
+export default Navbar;
